2bf21f5ee9c8235a11f9db3e247ea8e38fe8b4ee
Alex Arasawa <aarasawa@gmail.com>
2024-03-21 18:40:18 -0700

    Completed pico challenge packer

--- SPOJ/C#/life_universe_everything/obj/Debug/net8.0/life_universe_everything.AssemblyInfo.cs
+++ SPOJ/C#/life_universe_everything/obj/Debug/net8.0/life_universe_everything.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("life_universe_everything")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cd89f7c1ed0b679c6330dd9224b4c623cbd9e94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e341749ac257550a43e9a589860a6f085b79547c")]
 [assembly: System.Reflection.AssemblyProductAttribute("life_universe_everything")]
 [assembly: System.Reflection.AssemblyTitleAttribute("life_universe_everything")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
